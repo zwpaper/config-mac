af77cc02c1399329ae44d96217db3d1e06e8831f
zwPapEr <zw.paper@gmail.com>
2015-07-10 13:42:00 +0800

    update todo

--- hammerspoon/sessions_control.lua
+++ hammerspoon/sessions_control.lua
@@ -12,10 +12,16 @@ Author: PapEr (zw.paper@gmail.com)
 
 --[[
 ToDo:
+* Detect if there is a save file, if not create a default
 * Add a new session
 * delete a session
 * jump to a session easily
 * show windows in current session
+
+MayBe:
+* Change session to a table like
+	{{win, isFull}, {...}, name=name_of_session}
+	and use ipairs to find window
 --]]
 
 
