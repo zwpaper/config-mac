ef9d8ba11605c1d8459b2dc32c1db31ce413941d
zwPapEr <zw.paper@gmail.com>
2015-07-10 13:38:22 +0800

    add session control to init.lua

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,4 +1,5 @@
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
+	sessionsSave(sessions)
 	hs.reload()
 	hs.notify.new({title="Hammerspoon", informativeText='Config loaded'}):send():release()
 end)
