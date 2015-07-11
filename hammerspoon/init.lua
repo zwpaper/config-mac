2f937b0d66bef7ab7b338b7d4f6d4c1fe2118cda
zwPapEr <zw.paper@gmail.com>
2015-07-11 19:04:10 +0800

    move sessions control to another repo

--- hammerspoon/init.lua
+++
@@ -1,8 +0,0 @@
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
-	sessionsSave(sessions)
-	hs.reload()
-	hs.notify.new({title="Hammerspoon", informativeText='Config loaded'}):send():release()
-end)
-
---Sessions control--
-require("sessions_control")
