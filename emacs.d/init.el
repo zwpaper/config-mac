5cb2b519009b6ba4be79ab004fada2cf327d00f6
zwPapEr <zw.paper@gmail.com>
2015-08-26 14:07:46 +0800

    set the solarized theme both x and terminal

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,6 +1,9 @@
 ;; Vars
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
+(display-time-mode 1)
+(setq display-time-24hr-format t)
+(setq display-time-day-and-date t)
 
 
 ;; Python
@@ -45,11 +48,15 @@
 ;; Elpy
 (elpy-enable)
 
+
 ;; Theme
-(add-to-list 'load-path "~/.emacs.d/themes")
-(require 'color-theme)
-(color-theme-initialize)
-(color-theme-euphoria)
+(add-to-list 'custom-theme-load-path
+             "~/.emacs.d/themes/emacs-color-theme-solarized/")
+(set-frame-parameter nil 'background-mode 'dark)
+(set-terminal-parameter nil 'background-mode 'dark)
+; (if (display-graphic-p)
+(load-theme 'solarized)
+
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -62,4 +69,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("1157a4055504672be1df1232bed784ba575c60ab44d8e6c7b3800ae76b42f8bd" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "d9046dcd38624dbe0eb84605e77d165e24fdfca3a40c3b13f504728bab0bf99d" default)))
+ '(menu-bar-mode nil)
  '(tool-bar-mode nil))
